c643bc9e16fd96de1abe1d66d82c15f32889a01f
MarcOrigins <alessandro.marconi02@gmail.com>
2024-07-20 17:55:59 -0500

    Arreglos del footer, eliminaos logos de rrss

--- fe_web/src/components/Footer.jsx
+++ fe_web/src/components/Footer.jsx
@@ -12,36 +12,10 @@ const Footer = ({ className = "" }) => {
             src="/src/assets/images//logo.png"
           />
         </div>
-        <div className="landing-page-footer-socials">
-          <img
-            className="landing-page-social-icon"
-            loading="lazy"
-            alt="X Logo"
-            src="/src/assets/images//x-logo.svg"
-          />
-          <img
-            className="landing-page-social-icon"
-            loading="lazy"
-            alt="Instagram Logo"
-            src="/src/assets/images//logo-instagram.svg"
-          />
-          <img
-            className="landing-page-social-icon"
-            loading="lazy"
-            alt="YouTube Logo"
-            src="/src/assets/images//logo-youtube.svg"
-          />
-          <img
-            className="landing-page-social-icon"
-            loading="lazy"
-            alt="LinkedIn Logo"
-            src="/src/assets/images//linkedin.svg"
-          />
-        </div>
+
       </footer>
       <div className="landing-page-footer-links">
-        <div className="landing-page-footer-link">Recursos</div>
-        <div className="landing-page-footer-link">Ayuda</div>
+        <div className="landing-page-footer-link">PlaySeña ®</div>
       </div>
     </div>
   );
